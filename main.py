c5ac3b88df407fa44dedcb90449d2ea42bb41522
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-08 13:15:04 +0600

    Used time.sleep(1) to add 1 sec delay after each segment moves

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from turtle import Turtle, Screen
 import time
-#Setting up the screen:
+
+# Setting up the screen:
 screen = Screen()
-screen.setup(width=600, height=600) #sets the screen dimension
+screen.setup(width=600, height=600)  # sets the screen dimension
 screen.bgcolor("black")
 screen.title("Snake Game")
-screen.tracer(0) #tracer set to 0
+screen.tracer(0)  # tracer set to 0
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 segments = []
 
-#Creating the snake body
+# Creating the snake body
 for position in starting_positions:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
@@ -18,41 +19,11 @@ for position in starting_positions:
     new_segment.goto(position)
     segments.append(new_segment)
 
-
-
 game_is_on = True
 while game_is_on:
     for seg in segments:
         seg.forward(20)
         screen.update()  # screen updated after 3 objects have been created
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        time.sleep(1)  # adds 1 sec delay after each segment moves
 
 screen.exitonclick()
