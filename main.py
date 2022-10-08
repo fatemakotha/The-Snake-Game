9bf09927384bb623e07e791e8e9d9ba627adbc85
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-08 11:40:16 +0600

    Changed turtle name to segment_1

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ x_positions = [0, -20, -40]
 all_turtles = []
 
 for turtle_index in range(0, 3):
-    new_turtle = Turtle(shape="square")
-    new_turtle.color("white")
-    new_turtle.goto(x=x_positions[turtle_index], y=0)
+    segment_1 = Turtle(shape="square")
+    segment_1.color("white")
+    segment_1.goto(x=x_positions[turtle_index], y=0)
 
 
 
