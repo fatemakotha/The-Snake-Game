83fe235ab312fa40a57cc5d04f04d48bc60f938b
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-08 11:38:59 +0600

    Created three turtle objects

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ screen.title("Snake Game")
 x_positions = [0, -20, -40]
 all_turtles = []
 
+for turtle_index in range(0, 3):
+    new_turtle = Turtle(shape="square")
+    new_turtle.color("white")
+    new_turtle.goto(x=x_positions[turtle_index], y=0)
 
 
 
