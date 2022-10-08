dce09ebb4346c9c825ccf210781828e8004edf84
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-08 11:50:35 +0600

    Turtle starts moving using for loop

--- main.py
+++ main.py
@@ -6,21 +6,25 @@ screen.setup(width=600, height=600) #sets the screen dimension
 screen.bgcolor("black")
 screen.title("Snake Game")
 
-x_positions = [0, -20, -40]
-all_turtles = []
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+segments = []
+
+#Creating the snake body
+for position in starting_positions:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+game_is_on = True
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
 
 
-segment_1 = Turtle(shape="square")
-segment_1.color("white")
-segment_1.goto(x=0, y=0)
 
-segment_2 = Turtle(shape="square")
-segment_2.color("white")
-segment_2.goto(x=-20, y=0)
 
-segment_3 = Turtle(shape="square")
-segment_3.color("white")
-segment_3.goto(x=-40, y=0)
 
 
 
