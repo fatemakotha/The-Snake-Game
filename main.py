79afd41a05129f955f6c6bfe9c7510054901abd7
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-08 11:44:25 +0600

    Created three turtle object separately

--- main.py
+++ main.py
@@ -9,11 +9,18 @@ screen.title("Snake Game")
 x_positions = [0, -20, -40]
 all_turtles = []
 
-for turtle_index in range(0, 3):
-    segment_1 = Turtle(shape="square")
-    segment_1.color("white")
-    segment_1.goto(x=x_positions[turtle_index], y=0)
 
+segment_1 = Turtle(shape="square")
+segment_1.color("white")
+segment_1.goto(x=0, y=0)
+
+segment_2 = Turtle(shape="square")
+segment_2.color("white")
+segment_2.goto(x=-20, y=0)
+
+segment_3 = Turtle(shape="square")
+segment_3.color("white")
+segment_3.goto(x=-40, y=0)
 
 
 
