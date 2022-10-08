2864814cf32249e462de373637aec971bb52c0b1
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-08 11:29:13 +0600

    Commented

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 from turtle import Turtle, Screen
 
-
+#Setting up the screen:
 screen = Screen()
 screen.setup(width=600, height=600) #sets the screen dimension
 screen.bgcolor("black")
 screen.title("Snake Game")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
