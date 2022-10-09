609d31b08c7a51d6d27ca968ed4d3a2443cf991e
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-09 14:22:58 +0600

    Penup

--- main.py
+++ main.py
@@ -7,13 +7,19 @@ screen.title("SNAKE GAME") #set title for the screen
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-for each_snake in range(3):
-    snake = Turtle("square")
-    snake.color("white")
-    snake.goto(starting_positions[each_snake]) #turtle.goto() makes the turtle to go to the x and y coordinate thats specified
+segments = []
 
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position) #turtle.goto() makes the turtle to go to the x and y coordinate thats specified
+    segments.append(new_segment)
 
-
+game_is_on = True
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
 
 
 
