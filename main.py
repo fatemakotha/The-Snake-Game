27488e19ae9256a5c162eeabe992c8a184a23d1e
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-09 16:54:41 +0600

    scree.listen() activated

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ screen.tracer(0) #tracer takes a number as input and it turns the animation on/o
 
 snake = Snake()
 
+screen.listen() #starts listening
+screen.onkey(key="Up", fun=move_upwards) #calls the move_forward function when w is hit **
+screen.onkey(key="Down", fun=move_downwards) #calls the move_backwards function when s is hit **
+screen.onkey(key="Left", fun=turn_left) #calls the counter_clockwise function when a is hit **
+screen.onkey(key="Right", fun=turn_right) #calls the clockwise function when d is hit **
+
+
+
 game_is_on = True
 while game_is_on:
     screen.update() #when tracer is off, we use update() to tell program to refresh and redraw the screen
