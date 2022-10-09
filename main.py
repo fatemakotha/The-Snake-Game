06ed68fd3e35b1a6d7c3f61d11e11067d0a71522
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-09 14:07:46 +0600

    Commented

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from turtle import Turtle, Screen
 
 screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("SNAKE GAME")
+screen.setup(width=600, height=600) #screen' width and height set to 600pixels
+screen.bgcolor("black") #screen's bgcolor set to black
+screen.title("SNAKE GAME") #set title for the screen
 
 snake = Turtle("square")
 snake.color("white")
