6143803fb8cc4218fa0a7bd2c92a70d77bba3447
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-09 14:12:46 +0600

    Commented

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 for each_snake in range(3):
     snake = Turtle("square")
     snake.color("white")
-    snake.goto(starting_positions[each_snake])
+    snake.goto(starting_positions[each_snake]) #turtle.goto() makes the turtle to go to the x and y coordinate thats specified
 
 
 
