bff3d72534e674589a0cf18506e35579d17501a4
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-09 14:28:35 +0600

    Set tracer() to off

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ screen = Screen()
 screen.setup(width=600, height=600) #screen' width and height set to 600pixels
 screen.bgcolor("black") #screen's bgcolor set to black
 screen.title("SNAKE GAME") #set title for the screen
+screen.tracer(0) #tracer takes a number as input and it turns the animation on/off 0 = off, 1 = on
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
@@ -22,6 +23,7 @@ while game_is_on:
         seg.forward(20)
 
 
+#whern tracer is off, we use update() to tell program to refresh and redraw the screen
 
 
 
