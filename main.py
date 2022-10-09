f7936be88fe8df0b8a69e5746def7eb4da71d3ba
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-09 14:11:08 +0600

    Created 3 snake segments

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ screen.setup(width=600, height=600) #screen' width and height set to 600pixels
 screen.bgcolor("black") #screen's bgcolor set to black
 screen.title("SNAKE GAME") #set title for the screen
 
-snake = Turtle("square")
-snake.color("white")
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+for each_snake in range(3):
+    snake = Turtle("square")
+    snake.color("white")
+    snake.goto(starting_positions[each_snake])
 
 
 
